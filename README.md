7f0a4fd85a8fa016816323cbfe3073d4a882bcdd
sherms <sherms77@gmail.com>
2021-11-02 16:05:17 +1100

    modified:   README.md 	modified:   webcrawler.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # About
-Basic Python web crawler.
+- Python web crawler. 
+- Created from YouTube tutorial.
+
+# Links
+Three videos:
+- 1.https://www.youtube.com/watch?v=XjNm9bazxn8 
+- 2.https://www.youtube.com/watch?v=sVNJOiTBi_8 
+- 3.https://www.youtube.com/watch?v=pLHejmLB16o&t=601s 
--- webcrawler.py
+++ webcrawler.py
@@ -6,6 +6,8 @@ def blinds_spider(max_pages):
     while page <= max_pages:
         url = 'https://www.bunnings.com.au/search/products?q=venetian%20blinds&sort=BoostOrder&page=' + str(page)
         source_code = requests.get(url)
-        
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
 
-        
+        # up to 2:35 in video 2.
+        # trying to fing unique trait specific to title that we can tell BeautifulSoup to use.        
