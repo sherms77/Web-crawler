8c3743b05ef095564c843b0a34492c1f027f5a6c
sherms <sherms77@gmail.com>
2021-11-07 18:42:02 +1100

    modified:   webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -1,13 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+import requests # Requests module requests info from a webpage - connects to the webpage.
+from bs4 import BeautifulSoup # BeautifulSoup4 pulls data out of HTML and XML files.
 
-def blinds_spider(max_pages):
-    page = 1
-    while page <= max_pages:
-        url = 'https://www.bunnings.com.au/search/products?q=venetian%20blinds&sort=BoostOrder&page=' + str(page)
-        source_code = requests.get(url)
-        plain_text = source_code.text
+# blinds_spider function - core spider to crawl pages
+def blinds_spider(max_pages): # max pages paramater sets how many pages to crawl
+    page = 1 # Count initializer
+    
+    # While loop to crawl pages based on argument passed to page paramater.
+    while page <= max_pages: # while value in page is less than max pages execute loop
+        url = 'https://www.bunnings.com.au/search/products?q=venetian%20blinds&sort=BoostOrder&page=' + str(page) # url to crawl. Value from page counter converted to string and added to end of the url to specify page number on site to be crawled.
+        source_code = requests.get(url) # Connects to webpage in url variable and stores results in source_code variable.
+        plain_text = source_code.text # Takes the text from webpage (stuff required to crawl) and stores it in plain_text variable.
         soup = BeautifulSoup(plain_text)
+        
+        for link in soup.findAll('a', )
 
         # up to 2:35 in video 2.
-        # trying to fing unique trait specific to title that we can tell BeautifulSoup to use.        
+        # trying to find unique trait specific to title that we can tell BeautifulSoup to use.        
