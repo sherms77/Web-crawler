3f249a914cf4941fa4a633f520e4d9af6801f101
sherms77 <sherms77@gmail.com>
2021-11-28 09:28:18 +1100

    modified:   notes.md 	modified:   tests/htmlText.py

--- notes.md
+++ notes.md
@@ -7,7 +7,17 @@
 - 403 error: https://stackoverflow.com/questions/38489386/python-requests-403-forbidden 
 - Web crawler tutorial (freeCodeCamp): https://www.youtube.com/watch?v=SqvVm3QiQVk&t=824s
 - Extract plain text from a HTML object: https://www.kite.com/python/answers/how-to-extract-text-from-an-html-file-in-python
+- What .text does: https://www.codegrepper.com/code-examples/python/.text+in+python
 
 # Notes - Descending order
+281121: .text is a method that converts HTML code to text. When you use the output from the .text conversion in a Beautiful soup object, the output for both appears to be the same. 
+Ie: Two lines of code from the webcrawler:
+    a.plain_text = source_code.text
+    b.soup = BeautifulSoup(plain_text, "html.parser")
+If you print the output of both these files its the same.
+However, based on info at 0:50 in video 2, the point of line (b) is to:
+    - convert to a BeautifulSoup object, and
+    - convert the data to a format that BeautifulSoup can use to crawl through it, sift through it and sort through it.
+
 281121: Before I try to fix 403 error I will try to work out how to get just the text from a website. I want to see how that works to understand it better. Refer to file
 251121: Trying to fix 403 error when making site request. Line 11 - request.get() returns error.
--- tests/htmlText.py
+++ tests/htmlText.py
@@ -1,5 +1,5 @@
 # test how to get text from HTML object
-# url: https://en.wikipedia.org/wiki/Soft_drink
+# refer to note 281121 in notes.md for analysis
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,12 +9,9 @@ url = 'https://en.wikipedia.org/wiki/Soft_drink'
 source_code = requests.get(url) # Connects to webpage in url variable and stores results in source_code variable.
 # print(source_code) # see output for requests.get()
 
-# print('THIS IS THE OUTPUT FROM THE PLAIN TEXT VARIABLE: \n')
 plain_text = source_code.text # Original code from tutorial - Takes the text from webpage (stuff required to crawl) and stores it in plain_text variable.
-# print('THIS IS THE OUTPUT FROM THE PLAIN TEXT VARIABLE: \n')
-# print(plain_text) # this works - It gets all the text from the HTML object.
-# print('LINE BREAK')
 
+# saves output from plain_text to plainHTML.txt
 html_file = open('plainHTML.txt', 'w')
 html_file.write('THIS IS THE OUTPUT FROM THE PLAIN TEXT VARIABLE: \n')
 html_file.write('\n')
@@ -22,10 +19,8 @@ html_file.write(plain_text)
 html_file.close
 
 soup = BeautifulSoup(plain_text, "html.parser") # Converts text from webpage into a bs4 object and stores in soup variable. Formats it into bs4 format so it can sift through the links.
-# print('THIS IS THE OUTPUT FROM THE SOUP VARIABLE: \n')
-# print(soup)
-# print('LINE BREAK')
 
+# saves output from soup to soup.txt
 soup_file = open('soupFile.txt', 'w')
 soup_file.write('THIS IS THE OUTPUT FROM THE SOUP VARIABLE: \n')
 soup_file.write('\n')
